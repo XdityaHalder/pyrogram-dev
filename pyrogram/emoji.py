ba24d4158d8c6f874223e7b45234be53e0bf6544
Aditya Halder <116054200+XdityaHalder@users.noreply.github.com>
2024-02-19 08:02:13 +0530

    changed

--- pyrogram/emoji.py
+++ pyrogram/emoji.py
@@ -1,21 +1,20 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Pyrogram-Dev - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2024-present Aditya <https://github.com/AdityaHalder>
 #
-#  This file is part of Pyrogram.
+#  This file is part of Pyrogram-Dev.
 #
-#  Pyrogram is free software: you can redistribute it and/or modify
+#  Pyrogram-Dev is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published
 #  by the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Pyrogram is distributed in the hope that it will be useful,
+#  Pyrogram-Dev is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
+#  along with Pyrogram-Dev.  If not, see <http://www.gnu.org/licenses/>.
 GRINNING_FACE = "\U0001f600"
 GRINNING_FACE_WITH_BIG_EYES = "\U0001f603"
 GRINNING_FACE_WITH_SMILING_EYES = "\U0001f604"
--- pyrogram/methods/utilities/add_handler.py
+++ pyrogram/methods/utilities/add_handler.py
@@ -1,21 +1,20 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Pyrogram-Dev - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2024-present Aditya <https://github.com/AdityaHalder>
 #
-#  This file is part of Pyrogram.
+#  This file is part of Pyrogram-Dev.
 #
-#  Pyrogram is free software: you can redistribute it and/or modify
+#  Pyrogram-Dev is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published
 #  by the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Pyrogram is distributed in the hope that it will be useful,
+#  Pyrogram-Dev is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
+#  along with Pyrogram-Dev.  If not, see <http://www.gnu.org/licenses/>.
 import pyrogram
 from pyrogram.handlers import DisconnectHandler
 from pyrogram.handlers.handler import Handler
