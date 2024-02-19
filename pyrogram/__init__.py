f4f1aeeb94634aa319df9808e31a6d658f3d4c66
Aditya Halder <116054200+XdityaHalder@users.noreply.github.com>
2024-02-19 08:36:22 +0530

    changed

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -17,7 +17,7 @@
 #  along with Pyrogram-Dev.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = "2.1.13"
+__version__ = "2.1.0"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2024-present Aditya <https://github.com/AdityaHalder>"
 
